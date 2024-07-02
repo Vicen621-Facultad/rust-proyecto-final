4b25bafc11bff3f4813b5edd09ee70011a73586f
Vicen621 <71749981+vicen621@users.noreply.github.com>
2024-07-02 15:08:47 -0300

    Mockeado funciones para poder testear reporte

--- reportes/lib.rs
+++ reportes/lib.rs
@@ -3,11 +3,11 @@
 
 #[ink::contract]
 mod reportes {
-    use votacion::UserManager;
+    #[cfg(not(test))]
+    use votacion::{UserManager, ReportMessage};
     use votacion::Usuario;
     use votacion::VotacionRef;
     use votacion::VotacionError;
-    use votacion::ReportMessage;
     use ink::prelude::vec::Vec;
     type Result<T> = core::result::Result<T, VotacionError>;
 
@@ -70,41 +70,80 @@ mod reportes {
     // ASK: Preguntar si se tienen que guardar los datos o con solo devolverlos ya estaria
     #[ink(storage)]
     pub struct Reportes {
+        #[cfg(not(test))]
         votacion: VotacionRef
     }
 
     impl Reportes {
         /// Crea un nuevo contrato de reportes
         #[ink(constructor)]
+        #[cfg(not(test))]
         pub fn new(votacion: VotacionRef) -> Self {
             Self { 
                 votacion
             }
         }
 
-        /// Crea un nuevo contrato de reportes a partir del hash de la votacion
-        #[ink(constructor)]
-        pub fn new_hash(votacion_hash: Hash) -> Self {
-            let votacion = VotacionRef::new(Self::env().account_id())
-                .endowment(0)
-                .code_hash(votacion_hash)
-                .salt_bytes(AccountId::from([0x63; 32]))
-                .instantiate();
+        #[cfg(test)]
+        pub fn new() -> Self {
+            Self {}
+        }
+        
+        #[cfg(not(test))]
+        fn data_reporte_registro_votantes(&self, eleccion_id: u32) -> Result<Vec<AccountId>> {
+            self.votacion.reporte_registro_votantes(eleccion_id)
+        }
 
-            Self { votacion }
+        #[cfg(not(test))]
+        fn data_reporte_participacion(&self, eleccion_id: u32) -> Result<(u128, u128)> {
+            self.votacion.reporte_participacion(eleccion_id)
+        }
+
+        #[cfg(not(test))]
+        fn data_reporte_resultado(&self, eleccion_id: u32) -> Result<Vec<(AccountId, u32)>> {
+            self.votacion.reporte_resultado(eleccion_id)
+        }
+
+        #[cfg(not(test))]
+        fn get_usuario(&self, id: AccountId) -> Result<Usuario> {
+            self.votacion.get_usuario(id)
+        }
+
+        // Funcion mockeada para devolver los votantes de una eleccion
+        #[cfg(test)]
+        fn data_reporte_registro_votantes(&self, _eleccion_id: u32) -> Result<Vec<AccountId>> {
+            Ok(vec![AccountId::from([0x1; 32]), AccountId::from([0x2; 32]), AccountId::from([0x3; 32])])
+        }
+
+        // Funcion mockeada para devolver la informacion de la participacion de una eleccion
+        #[cfg(test)]
+        fn data_reporte_participacion(&self, _eleccion_id: u32) -> Result<(u128, u128)> {
+            Ok((10, 4))
+        }
+
+        // Funcion mockeada para devolver la informacion del resultado de una eleccion
+        #[cfg(test)]
+        fn data_reporte_resultado(&self, _eleccion_id: u32) -> Result<Vec<(AccountId, u32)>> {
+            Ok(vec![(AccountId::from([0x1;32]), 2), (AccountId::from([0x2;32]), 0), (AccountId::from([0x3;32]), 1)])
+        }
+
+        // Funcion mockeada para devolver un usuario
+        #[cfg(test)]
+        fn get_usuario(&self, id: AccountId) -> Result<Usuario> {
+            Ok(Usuario::new(id, "test".to_string(), "test".to_string(), "direccion".to_string(), "12345678".to_string(), 18))
         }
 
         /// Devuelve el reporte de registro de votantes de una eleccion
         #[ink(message)]
         pub fn reporte_registro_votantes(&self, eleccion_id: u32) -> Result<DataRegistroVotantes> {
-            let id_votantes = self.votacion.reporte_registro_votantes(eleccion_id)?;
+            let id_votantes = self.data_reporte_registro_votantes(eleccion_id)?;
             let mut usuarios_votantes = Vec::new();
 
             // Itero sobre los id de los votantes para recuperar su usuario en el sistema y devolverlo en el reporte
             // Jamas deberia dar error el get_usuario(id) debido a que se verifica siempre que sean usuarios
             // aceptados aquellos que se los acepte como votantes y los candidadtos
             for id in id_votantes {
-                usuarios_votantes.push(self.votacion.get_usuario(id)?);
+                usuarios_votantes.push(self.get_usuario(id)?);
             }
 
             Ok(DataRegistroVotantes::new(usuarios_votantes))
@@ -113,7 +152,7 @@ mod reportes {
         /// Devuelve el reporte de participacion de una eleccion
         #[ink(message)]
         pub fn reporte_participacion(&self, eleccion_id: u32) -> Result<DataParticipacion> {
-            let data = self.votacion.reporte_participacion(eleccion_id)?;
+            let data = self.data_reporte_participacion(eleccion_id)?;
             let num_votantes = data.0;
             let num_votantes_voto = data.1;
 
@@ -128,7 +167,7 @@ mod reportes {
         /// Devuelve el reporte de resultado de una eleccion
         #[ink(message)]
         pub fn reporte_resultado(&self, eleccion_id: u32) -> Result<DataResultado> {
-            let mut data = self.votacion.reporte_resultado(eleccion_id)?;
+            let mut data = self.data_reporte_resultado(eleccion_id)?;
             data.sort_by_key(|(_, voto)| *voto);
             Ok(DataResultado::new(data))
         }
@@ -156,40 +195,30 @@ mod reportes {
             let data = DataResultado::new(vec![(AccountId::from([0x1; 32]), 1)]);
             assert_eq!(data.resultado.len(), 1);
         }
-    }
 
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        use super::*;
-        use std::fmt::Write;
-        use ink_e2e::ContractsBackend;
+        #[test]
+        fn test_reporte_registro_votantes() {
+            let reportes = Reportes::new();
+            let data = reportes.reporte_registro_votantes(1).unwrap();
+            assert_eq!(data.votantes.len(), 3);
+        }
 
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+        #[test]
+        fn test_reporte_participacion() {
+            let reportes = Reportes::new();
+            let data = reportes.reporte_participacion(1).unwrap();
+            assert_eq!(data.votos, 4);
+            assert_eq!(data.participacion, 40);
+        }
 
-        #[ink_e2e::test]
-        async fn test_reporte_registro_votantes<Client: E2EBackend>(
-            mut client: Client,
-        ) -> E2EResult<()> {
-            let votacion = client
-                .upload("votacion", &ink_e2e::alice())
-                .submit()
-                .await
-                .expect("votacion `accumulator` failed")
-                .code_hash;
-            let mut constructor = ReportesRef::new_hash(votacion);
-
-            let reportes = client
-                .instantiate("reportes", &ink_e2e::alice(), &mut constructor)
-                .submit()
-                .await
-                .expect("instantiate failed");
-
-            let mut call_builder =
-                reportes.call_builder::<Reportes>();
-
-            let reporte_registro_votantes = call_builder.reporte_registro_votantes(0);
-
-            Ok(())
+        #[test]
+        fn test_reporte_resultado() {
+            let reportes = Reportes::new();
+            let mut data = reportes.reporte_resultado(1).unwrap();
+            assert_eq!(data.resultado.len(), 3);
+            assert_eq!(data.resultado.pop(), Some((AccountId::from([0x1; 32]), 2)));
+            assert_eq!(data.resultado.pop(), Some((AccountId::from([0x3; 32]), 1)));
+            assert_eq!(data.resultado.pop(), Some((AccountId::from([0x2; 32]), 0)));
         }
     }
 }
